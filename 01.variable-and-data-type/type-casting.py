7ad9562a8b5892f457e6295333042ba4e89bb377
dev-rashedin <rashedinislam.06@gmail.com>
2026-09-05 21:31:34 +0600

    variable explored

---
+++ 01.variable-and-data-type/type-casting.py
@@ -0,0 +1,22 @@
+# Typecasting --> the process of converting a variable from one type to another str(), int(), float(), bool()
+
+name = 'Rashedin'
+age = 29
+gpa = 3.2
+
+is_present = True
+
+# type(age)
+
+age = float(age)
+gpa = int(gpa)
+
+age = str(age)
+
+print(age + '1')
+
+print(type(age))
+
+name = bool(name)
+
+print(name)
---
+++ 01.variable-and-data-type/variable.py
@@ -0,0 +1,64 @@
+# Variable = A container for a value (string, number, object, etc.)
+# Variables are case-sensitive
+
+first_name = "Rashedin"
+last_name = "Islam"
+full_name = f"My name is {first_name} {last_name}"
+
+print(full_name)
+
+food = "Pizza"
+food = "Sushi"
+print(f"I like {food}")
+
+email = 'rashedinislam@.com'
+print(f"my email is {email}")
+
+# All of them are example of string
+
+
+# Integer
+age = 29
+
+quantity = 3
+
+num_of_students = 30
+
+print(f"I am {age} years old")
+
+print(f"I have {quantity} apples")
+
+print(f"I have {num_of_students} students")
+
+
+# Float
+price = 10.98
+print(f"The price is ${price}")
+
+gpa = 3.9
+print(f"My gpa is {gpa}")
+
+distance = 1.5
+print(f"The distance is {distance} km")
+
+
+# Boolean
+is_new = True
+is_hot = False
+
+print(f"The temperature is hot: {is_hot}")
+
+is_student = True
+
+if is_student:
+  print("I am a student")
+else:
+  print("I am not a student")
+
+
+for_sale = False
+
+if for_sale:
+  print("The item is for sale")
+else:
+  print("The item is not for sale")
