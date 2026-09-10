133be836db5301491a5820d3447d112ab2afbb10
dev-rashedin <rashedinislam.06@gmail.com>
2026-09-10 16:59:11 +0600

    updated

---
+++ 02.math/arithmetic-operator.py
@@ -0,0 +1,19 @@
+friends = 0
+
+friends = friends + 1
+friends += 1 ## this is known as augmented assignment
+
+friends -= 1
+
+friends *= 6
+friends /= 3
+
+# friends = friends ** 2
+
+friends **= 2
+
+# reminder = friends % 3
+
+friends %= 3
+
+print(friends)
